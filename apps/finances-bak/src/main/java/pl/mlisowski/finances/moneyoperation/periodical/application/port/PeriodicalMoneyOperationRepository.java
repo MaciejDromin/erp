0a28934f658ffa2b28d87af8c4416b9c73e1169e
Maciej Lisowski <macieejl00@gmail.com>
2023-08-01 21:09:09 +0200

    Init new project, removed trash
    Signed-off-by: Maciej Lisowski <macieejl00@gmail.com>

--- apps/finances-bak/src/main/java/pl/mlisowski/finances/moneyoperation/periodical/application/port/PeriodicalMoneyOperationRepository.java
+++
@@ -1,9 +0,0 @@
-package pl.mlisowski.finances.moneyoperation.periodical.application.port;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pl.mlisowski.finances.moneyoperation.periodical.domain.PeriodicalMoneyOperaion;
-
-@Repository
-public interface PeriodicalMoneyOperationRepository extends JpaRepository<PeriodicalMoneyOperaion, String> {
-}
