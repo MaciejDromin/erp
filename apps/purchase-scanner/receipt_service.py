b2a3eca130414a96266dac99eaa0752ea3864c91
Maciej Lisowski <macieejl00@gmail.com>
2024-05-15 02:00:42 +0200

    Initial FastAPI project
    Signed-off-by: Maciej Lisowski <macieejl00@gmail.com>

---
+++ apps/purchase-scanner/receipt_service.py
@@ -0,0 +1,21 @@
+from file_manager import save_locally, unpack_archive, clean_directories
+from receipt_scan import scan_receipts, parse_receipts
+from fastapi import UploadFile
+import json
+
+async def handle_uploaded_receipts(filename, file_content):
+    saved_file = await save_locally(filename, file_content)
+    #archive_receipts(saved_file)
+    receipts = unpack_archive(saved_file)
+    print(receipts)
+    scanned_receipts = scan_receipts(receipts)
+    parsed_receipts = parse_receipts(scanned_receipts)
+    #for pr in parsed_receipts:
+        #print(pr)
+    #    print(json.dumps(pr, indent=2))
+    #filtered_receipts = filter_receipts()
+
+    #publish("topic", filtered_receipts)
+
+    clean_directories()
+
---
+++ apps/purchase-scanner/server.py
@@ -0,0 +1,17 @@
+from typing import Union
+from receipt_service import handle_uploaded_receipts
+from fastapi import FastAPI, File, UploadFile
+import asyncio
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.post("/receipts")
+async def upload_file(file: UploadFile):
+    asyncio.ensure_future(handle_uploaded_receipts(file.filename, await file.read()))
+    return {"filename": file.filename, "status": "PROCESSING"}
