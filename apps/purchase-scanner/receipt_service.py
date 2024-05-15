d66f6b128ce27f5bbb8d10b3f25bd3253dd35679
Maciej Lisowski <macieejl00@gmail.com>
2024-05-15 02:25:16 +0200

    Added basic sftp support
    Signed-off-by: Maciej Lisowski <macieejl00@gmail.com>

--- apps/purchase-scanner/receipt_service.py
+++ apps/purchase-scanner/receipt_service.py
@@ -1,11 +1,12 @@
 from file_manager import save_locally, unpack_archive, clean_directories
 from receipt_scan import scan_receipts, parse_receipts
+from sftp_connector import archive_receipts
 from fastapi import UploadFile
 import json
 
 async def handle_uploaded_receipts(filename, file_content):
     saved_file = await save_locally(filename, file_content)
-    #archive_receipts(saved_file)
+    archive_receipts(saved_file)
     receipts = unpack_archive(saved_file)
     print(receipts)
     scanned_receipts = scan_receipts(receipts)
