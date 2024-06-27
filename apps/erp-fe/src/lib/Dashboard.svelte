8d7b547cd7a0a681215b9157b58fc22c9bf8d2fa
Maciej Lisowski <macieejl00@gmail.com>
2024-06-27 22:41:38 +0200

    Improve drag and drop, added placeholder
    Signed-off-by: Maciej Lisowski <macieejl00@gmail.com>

--- apps/erp-fe/src/lib/Dashboard.svelte
+++ apps/erp-fe/src/lib/Dashboard.svelte
@@ -1,81 +1,97 @@
 <script>
+  TODO: get items on initial dashboard open
   let items = [
     { label: 'Item 1', x: 0, y: 0 },
+    { label: 'Item 4', x: 0, y: 1 },
     { label: 'Item 2', x: 1, y: 0 },
     { label: 'Item 3', x: 2, y: 0 },
-    { label: 'Item 4', x: 0, y: 1 },
     // ... other items with their respective coordinates
   ];
 
   const onDragStart = (event, item) => {
     event.dataTransfer.setData('text/plain', JSON.stringify({ label: item.label, x: item.x, 
 y: item.y }));
-    event.target.classList.add('is-moving');
   };
 
-  const onDrop = (event, targetColumn) => {
+  const onDrop = (event) => {
     const data = JSON.parse(event.dataTransfer.getData('text'));
-    items[targetColumn].label = data.label;
-    items[targetColumn].x = data.x;
-    items[targetColumn].y = data.y;
+    const targetAttr = event.target.attributes;
+    const targetRow = Number(targetAttr.y.value);
+    const targetColumn = Number(targetAttr.x.value);
+    items.forEach((item) => {
+      if (item.label === data.label) {
+        item.x = targetColumn - 1
+        item.y = targetRow - 1
+      }
+    })
+    items = items;
     updateItems();
-    event.target.classList.remove('is-moving');
+    document.querySelectorAll('.placeholder').forEach((placeHolder) => {
+        placeHolder.remove();
+    });
   };
 
   const onDragOver = (event) => {
     event.preventDefault()
-    const targetRow = event.currentTarget.closest('.grid-row').dataset.row;
-    const targetColumn = Number(event.target.parentNode.parentNode.dataset.column);
+    const targetStyle = event.target.attributes.style.value;
+    const targetRow = targetStyle.charAt(targetStyle.length - 1);
+    const targetColumn = targetStyle.charAt(targetStyle.indexOf(";") - 1);
+    dropPlaceholder(targetRow, targetColumn)
+  };
+
+  const dropPlaceholder = (targetRow, targetColumn) => {
     const placeholder = document.createElement('div');
     document.querySelectorAll('.placeholder').forEach((placeHolder) => {
         placeHolder.remove();
     });
     placeholder.classList.add('placeholder');
-    placeholder.style.gridColumnStart = targetColumn + 1;
-    placeholder.style.gridRowStart = Number(targetRow) + 1;
-    event.target.parentNode.append(placeholder);  };
+    placeholder.style.cssText = `grid-column: ${targetColumn}; grid-row: ${targetRow}`
+    placeholder.style.border = "4px solid red";
+    placeholder.style.opacity = "0.5";
+    placeholder.setAttribute('x', targetColumn)
+    placeholder.setAttribute('y', targetRow)
+    placeholder.addEventListener("drop", function(e) {
+      onDrop(e)
+    })
+    placeholder.addEventListener("dragover", function(e) {
+      e.preventDefault();
+    })
+
+    event.target.parentNode.append(placeholder);
+  }
 
   const updateItems = () => {
     // Update the items array based on user drag-and-drop actions
     console.log('items:', items);
+    // TODO: here update items locations
+    // TODO: at the end send new locations to backend
   };
 </script>
 
 <div class="flex flex-wrap dnd-grid">
-  {#each items as item, index (index)}
+  {#each items as item}
     <div draggable={true} on:dragstart={() => onDragStart(event, item)} on:drop={() => 
-onDrop(event, index)} on:dragover={onDragOver} class="drag-item" style={`grid-column: 
+onDrop(event)} on:dragover={onDragOver} class="drag-item" style={`grid-column: 
 ${item.x + 1}; grid-row: ${item.y + 1}`}>
       {item.label}
     </div>
   {/each}
 </div>
-<style>
+<style global>
 .dnd-grid {
   display: grid;
-  grid-template-columns: auto auto auto;
+  grid-template-columns: 32% 32% 32%;
   gap: 20px;
 }
 
 .drag-item {
   border: 1px solid #ccc;
   padding: 10px;
-  margin-bottom: 10px;
   cursor: move;
   transition: transform 0.3s ease-in-out;
 }
 
 .drag-item:hover {
   background-color: #f5f5f5;
-}
-
-.is-moving {
-  will-change: transform;
-}
-
-.placeholder {
-  background-color: #ddd;
-  border: 1px solid #ddd;
-  opacity: 0.5;
 }
 </style>
