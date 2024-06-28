c653af8ee05c50e5a0256fa2d832ee0a1b77a206
Maciej Lisowski <macieejl00@gmail.com>
2024-06-28 18:40:05 +0200

    Created update mechanism form grid items
    Signed-off-by: Maciej Lisowski <macieejl00@gmail.com>

--- apps/erp-fe/src/lib/Dashboard.svelte
+++ apps/erp-fe/src/lib/Dashboard.svelte
@@ -5,6 +5,7 @@
     { label: 'Item 4', x: 0, y: 1 },
     { label: 'Item 2', x: 1, y: 0 },
     { label: 'Item 3', x: 2, y: 0 },
+    { label: 'Item 6', x: 1, y: 1 },
     // ... other items with their respective coordinates
   ];
 
@@ -18,14 +19,7 @@ y: item.y }));
     const targetAttr = event.target.attributes;
     const targetRow = Number(targetAttr.y.value);
     const targetColumn = Number(targetAttr.x.value);
-    items.forEach((item) => {
-      if (item.label === data.label) {
-        item.x = targetColumn - 1
-        item.y = targetRow - 1
-      }
-    })
-    items = items;
-    updateItems();
+    updateItems(data, targetColumn - 1, targetRow - 1);
     document.querySelectorAll('.placeholder').forEach((placeHolder) => {
         placeHolder.remove();
     });
@@ -46,7 +40,7 @@ y: item.y }));
     });
     placeholder.classList.add('placeholder');
     placeholder.style.cssText = `grid-column: ${targetColumn}; grid-row: ${targetRow}`
-    placeholder.style.border = "4px solid red";
+    placeholder.style.border = "4px solid #b387fa";
     placeholder.style.opacity = "0.5";
     placeholder.setAttribute('x', targetColumn)
     placeholder.setAttribute('y', targetRow)
@@ -60,12 +54,114 @@ y: item.y }));
     event.target.parentNode.append(placeholder);
   }
 
-  const updateItems = () => {
+  const updateItems = (data, targetColumn, targetRow) => {
     // Update the items array based on user drag-and-drop actions
-    console.log('items:', items);
-    // TODO: here update items locations
+    if (isNeighbour(data.x, data.y, targetColumn, targetRow)) {
+      items.forEach((item) => {
+        if (item.label === data.label) {
+          item.x = targetColumn
+          item.y = targetRow
+          return;
+        }
+        if (item.x === targetColumn && item.y === targetRow) {
+          item.x = data.x;
+          item.y = data.y;
+          return;
+        }
+      })
+      items = items;
+      return;
+    }
+    if ((targetRow === data.y && data.x < targetColumn)
+      || targetRow > data.y) {
+      items.forEach((item) => {
+        if (item.label === data.label) {
+          item.x = targetColumn
+          item.y = targetRow
+          return;
+        }
+        if (item.x === targetColumn && item.y === targetRow) {
+          item.x = data.x;
+          item.y = data.y;
+          return;
+        }
+      })
+      items = items;
+      return;
+    }
+    items.forEach((item) => {
+      if (item.label === data.label) {
+        item.x = targetColumn
+        item.y = targetRow
+        return;
+      }
+      if (item.y === targetRow && item.x === targetColumn) {
+        if (isNeighbour(item.x, item.y, data.x, data.y)) {
+          item.x = data.x;
+          item.y = data.y;
+          return;
+        }
+        const position = determineNewPosition(item.y, item.x);
+        item.x = position.x;
+        item.y = position.y;
+        return;
+      }
+      if (isBetween({ x: item.x, y: item.y }, positionsInOrder(data.x, data.y, targetColumn, targetRow))) {
+        const position = determineNewPosition(item.y, item.x);
+        item.x = position.x;
+        item.y = position.y;
+      }
+    })
+    items = items;
     // TODO: at the end send new locations to backend
   };
+
+  const positionsInOrder = (dataX, dataY, targetColumn, targetRow) => {
+    if (dataY === targetRow) {
+      const xOrder = dataX > targetColumn ? { start: targetColumn, end: dataX }
+        : { start: dataX, end: targetColumn }
+      return { 
+        start: { x: xOrder.start, y: targetRow },
+        end: { x: xOrder.end, y: targetRow }
+      }
+    }
+    if (targetRow > dataY) {
+      return {
+        start: { x: dataX, y: dataY },
+        end: { x: targetColumn, y: targetRow }
+      }
+    }
+    return {
+      start: { x: targetColumn, y: targetRow },
+      end: { x: dataX, y: dataY }
+    }
+  }
+
+  const isBetween = (itemPos, pointsPos) => {
+    if (pointsPos.start.y === pointsPos.end.y) {
+      if (itemPos.x > pointsPos.start.x && itemPos.x <= pointsPos.end.x) return true;
+      return false;
+    }
+    if (itemPos.y === pointsPos.start.y && itemPos.x > pointsPos.start.x) return true;
+    else if (itemPos.y === pointsPos.start.y) return false;
+    if (itemPos.y <= pointsPos.end.y && itemPos.x <= pointsPos.end.x) return true;
+    return false;
+  }
+
+  const isNeighbour = (itemX, itemY, targetX, targetY) => {
+    if (itemY === targetY && itemX + 1 === targetX) return true;
+    if (itemY === targetY && itemX - 1 === targetX) return true;
+    if (itemX === 0 && itemY - 1 === targetY && 2 === targetX) return true;
+    if (itemX === 2 && itemY + 1 === targetY && 0 === targetX) return true;
+    return false;
+  }
+
+  const determineNewPosition = (itemY, itemX) => {
+    if (itemX + 1 >= 3) {
+      return { x: 0, y: itemY + 1 }
+    }
+    return { x: itemX + 1, y: itemY }
+  }
 </script>
 
 <div class="flex flex-wrap dnd-grid">
