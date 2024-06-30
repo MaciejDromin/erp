9c89948c17137f9d28a37e903f9999eda29aae56
Maciej Lisowski <macieejl00@gmail.com>
2024-06-30 12:18:12 +0200

    Remove changed service name
    Signed-off-by: Maciej Lisowski <macieejl00@gmail.com>

--- apps/erp-fe/src/lib/scripts/urls.ts
+++ apps/erp-fe/src/lib/scripts/urls.ts
@@ -3,6 +3,6 @@ const INVENTORY_URL = "http://inventory:8080"
 const PLANNER_URL = "http://planner:8080"
 const CONSUL_URL = "http://erp-consul:8500"
 const PURCHASE_SCANNER_URL = "http://purchase-scanner:80"
-const DASHBOARD_URL = "http://localhost:8083"
+const DASHBOARD_URL = "http://dashboard:8080"
 
 export { FINANCES_URL, INVENTORY_URL, PLANNER_URL, CONSUL_URL, PURCHASE_SCANNER_URL, DASHBOARD_URL }
