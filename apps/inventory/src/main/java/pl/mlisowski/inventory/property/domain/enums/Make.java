b9e4e16d7d5285f5de33419a940e22416db1aced
Maciej Lisowski <macieejl00@gmail.com>
2024-07-25 21:23:23 +0200

    Modelled vehicle, added enums
    Signed-off-by: Maciej Lisowski <macieejl00@gmail.com>

---
+++ apps/inventory/src/main/java/pl/mlisowski/inventory/property/domain/enums/Make.java
@@ -0,0 +1,43 @@
+package pl.mlisowski.inventory.property.domain.enums;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+
+public enum Make {
+
+    BMW("BMW"),
+    VOLKSWAGEN("Volkswagen"),
+    AUDI("Audi"),
+    FORD("Ford"),
+    MERCEDES_BENZ("Mercedes-Benz"),
+    OPEL("Opel"),
+    SKODA("Skoda"),
+    RENAULT("Renault"),
+    PEUGEOT("Peugeot"),
+    ALFA_ROMEO("Alfa Romeo"),
+    VOLVO("Volvo"),
+    FIAT("Fiat"),
+    TESLA("Tesla"),
+    JAGUAR("Jaguar"),
+    JEEP("Jeep"),
+    KIA("Kia"),
+    LAND_ROVER("Land Rover"),
+    LEXUS("Lexus"),
+    MAZDA("Mazda"),
+    NISSAN("Nissan"),
+    DODGE("Dodge"),
+    SUBARU("Subaru"),
+    SUZUKI("Suzuki"),
+    TOYOTA("Toyota");
+
+    private final String name;
+
+    Make(String name) {
+        this.name = name;
+    }
+
+    @JsonValue
+    public String getName() {
+        return name;
+    }
+
+}
--- apps/inventory/src/main/java/pl/mlisowski/inventory/vehicle/domain/Vehicle.java
+++ apps/inventory/src/main/java/pl/mlisowski/inventory/vehicle/domain/Vehicle.java
@@ -8,6 +8,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.bson.types.ObjectId;
+import pl.mlisowski.inventory.property.domain.enums.BodyStyle;
+import pl.mlisowski.inventory.property.domain.enums.DriveTrain;
+import pl.mlisowski.inventory.property.domain.enums.FuelType;
+import pl.mlisowski.inventory.property.domain.enums.Make;
+import pl.mlisowski.inventory.property.domain.enums.Transmission;
 
 @Getter
 @Setter
@@ -18,4 +24,18 @@ import lombok.ToString;
 @ToString
 @EqualsAndHashCode(of = "id")
 public class Vehicle {
+
+    private ObjectId id;
+    private String name;
+    private int year;
+    private int odometer;
+    private BodyStyle bodyStyle;
+    private Make make;
+    private String model;
+    private FuelType fuelType;
+    private DriveTrain driveTrain;
+    private Transmission transmission;
+    private String engineType;
+    private String vin;
+
 }
