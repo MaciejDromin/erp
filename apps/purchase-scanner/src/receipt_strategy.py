256c382636632e943ed289c31e9668757f38f1dc
Maciej Lisowski <macieejl00@gmail.com>
2024-08-08 12:13:48 +0200

    Changed approach to receipt scanning
    Signed-off-by: Maciej Lisowski <macieejl00@gmail.com>

--- apps/purchase-scanner/src/receipt_strategy.py
+++ apps/purchase-scanner/src/receipt_strategy.py
@@ -1,7 +1,8 @@
-from imutils.perspective import four_point_transform
+import regex as re
+import matplotlib.pyplot as plt
+import numpy as np
 import scanning_strategy
 import pytesseract
-import regex as re
 import argparse
 import imutils
 import cv2
@@ -19,23 +20,61 @@ class ReceiptStrategy(scanning_strategy.ScanningStrategy):
     QUANTITY_UNIT_PATTERN = r'[\s|~]\d+,{0,1}\d{0,4}(SZT|KG|L){0,1}$'
     UNIT_PATTERN = r'(SZT|KG|L)'
     EXTENSION = "jpg"
-    
+
+
+    def opencv_resize(self, image, ratio):
+        width = int(image.shape[1] * ratio)
+        height = int(image.shape[0] * ratio)
+        dim = (width, height)
+        return cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
+
 
     def get_contours_from_receipt(self, receipt):
-        orig = cv2.imread(receipt)
-        image = orig.copy()
-        image = imutils.resize(image, width=500)
-        ratio = orig.shape[1] / float(image.shape[1])
+        image = cv2.imread(receipt)
+        orig = image.copy()
+        ratio = 500 / image.shape[0]
 
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        blurred = cv2.GaussianBlur(gray, (5, 5,), 0)
-        edged = cv2.Canny(blurred, 75, 200)
-
-        cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
-            cv2.CHAIN_APPROX_SIMPLE)
-        cnts = imutils.grab_contours(cnts)
-        return orig, ratio, sorted(cnts, key=cv2.contourArea, reverse=True)
+        image = self.opencv_resize(image, ratio)
         
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+        rectKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 9))
+        dilated = cv2.dilate(blurred, rectKernel)
+
+        edged = cv2.Canny(dilated, 100, 200, apertureSize=3)
+        cnts, hierarchy = cv2.findContours(edged, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        sorted_cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:5]
+
+        return orig, ratio, sorted_cnts
+
+
+    def contour_to_rect(self, contour, ratio):
+        pts = contour.reshape(4, 2)
+        rect = np.zeros((4, 2), dtype = "float32")
+        s = pts.sum(axis = 1)
+        rect[0] = pts[np.argmin(s)]
+        rect[2] = pts[np.argmax(s)]
+        diff = np.diff(pts, axis = 1)
+        rect[1] = pts[np.argmin(diff)]
+        rect[3] = pts[np.argmax(diff)]
+        return rect / ratio
+
+
+    def wrap_perspective(self, img, rect):
+        (tl, tr, br, bl) = rect
+        widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+        widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+        heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+        heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+        maxWidth = max(int(widthA), int(widthB))
+        maxHeight = max(int(heightA), int(heightB))
+        dst = np.array([
+            [0, 0],
+            [maxWidth - 1, 0],
+            [maxWidth - 1, maxHeight - 1],
+            [0, maxHeight - 1]], dtype = "float32")
+        M = cv2.getPerspectiveTransform(rect, dst)
+        return cv2.warpPerspective(img, M, (maxWidth, maxHeight))
 
 
     def scan_receipts(self, receipts):
@@ -46,20 +85,21 @@ class ReceiptStrategy(scanning_strategy.ScanningStrategy):
             receiptCnt = None
 
             for c in cnts:
-	            peri = cv2.arcLength(c, True)
-	            approx = cv2.approxPolyDP(c, 0.032 * peri, True)
-	            if len(approx) == 4:
-		            receiptCnt = approx
-		            break
+                peri = cv2.arcLength(c, True)
+                approx = cv2.approxPolyDP(c, 0.032 * peri, True)
+
+                if len(approx) == 4:
+                    receiptCnt = approx
+                    break
             if receiptCnt is None:
                 print("No outline found for receipt: " + receipt + " skipping...")
                 # TODO: send kafka message
                 continue
 
-            receiptTransformed = four_point_transform(orig, receiptCnt.reshape(4, 2) * ratio)
+            scanned = self.wrap_perspective(orig.copy(), self.contour_to_rect(receiptCnt, ratio))
 
             text = pytesseract.image_to_string(
-                cv2.cvtColor(receiptTransformed, cv2.COLOR_BGR2RGB), config="--psm 4", lang="pol")
+                cv2.cvtColor(scanned, cv2.COLOR_BGR2GRAY), config="--psm 6", lang="pol")
 
             parsed_receipt = {
                 "data": text,
@@ -85,7 +125,7 @@ class ReceiptStrategy(scanning_strategy.ScanningStrategy):
     def convert_to_double(self, value):
         if isinstance(value, float):
             return value
-        result = re.sub(r'[a-zA-Z*«+]', '', value)
+        result = re.sub(r'[a-zA-Z*«+\s]', '', value)
         result = re.sub(r',', '.', result)
         return float(result)
 
