4aa9cc1429dd73418c6a5ea7cf1844336ebe71b8
Maciej Lisowski <macieejl00@gmail.com>
2024-09-26 21:00:50 +0200

    Big Improvements

---
+++ apps/erp-fe/src/lib/finances/money-operation/MoneyOperationAddView.svelte
@@ -0,0 +1,86 @@
+<script lang="ts">
+  import { MoneyOperationType } from '$lib/finances/types/financialTypes'
+  import { operationCategoriesStore } from '$lib/finances/stores/selectedOperationCategory'
+  import { onMount } from 'svelte'
+  import Modal from '$lib/Modal.svelte'
+  import OperationCategoryTable from '$lib/finances/operation-category/OperationCategoryTable.svelte'
+  import Pageable from '$lib/Pageable.svelte'
+
+  let operationType = MoneyOperationType.INCOME
+
+  let categories: any[] = []
+
+  onMount(() => {
+    $operationCategoriesStore = []
+  })
+
+  operationCategoriesStore.subscribe((cat) => {
+    categories = [...cat]
+    categories = categories
+  })
+
+  const determineButtonName = (arr: any[]): string => {
+    if (arr.length === 0) return 'select category'
+    return `${arr.length} category selected`
+  }
+</script>
+
+<form method="POST" class="mx-auto flex flex-col gap-3">
+  <div class="flex flex-row gap-3">
+    <input
+      name="amount"
+      type="text"
+      placeholder="Amount"
+      class="input input-bordered input-primary w-full max-w-xs"
+    />
+    <input
+      name="currencyCode"
+      type="text"
+      placeholder="Currency Code"
+      class="input input-bordered input-primary w-full max-w-xs"
+    />
+  </div>
+  <div class="flex flex-row gap-3">
+    <input
+      name="operationDescription"
+      type="text"
+      placeholder="Operation Description"
+      class="input input-bordered input-primary w-full"
+    />
+  </div>
+
+  <div class="flex flex-row gap-3">
+    <select
+      name="operationType"
+      class="select select-primary w-full max-w-xs"
+      bind:value={operationType}
+    >
+      {#each Object.values(MoneyOperationType) as operationType}
+        <option value={operationType}>{operationType}</option>
+      {/each}
+    </select>
+    <select
+      multiple
+      name="categoryId"
+      class="p-4 mr-auto hidden"
+      bind:value={categories}
+    >
+      {#each categories as category}
+        <option value={category}></option>
+      {/each}
+    </select>
+    <div class="mr-auto">
+      <Modal
+        modalId="category_modal"
+        buttonName={determineButtonName(categories)}
+      >
+        <Pageable
+          endpoint="/finances/operation-category"
+          component={OperationCategoryTable}
+          additionalSearch={`&operationType=${operationType}`}
+        />
+      </Modal>
+    </div>
+  </div>
+  <button class="btn btn-primary mx-auto">Add</button>
+</form>
