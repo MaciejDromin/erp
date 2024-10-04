28272dbdfa2546df02b2d2af5a90254f35ddee46
Maciej Lisowski <macieejl00@gmail.com>
2024-10-04 17:04:51 +0200

    handle vehicles

--- apps/erp-fe/src/routes/inventory/vehicles/add/+page.server.ts
+++ apps/erp-fe/src/routes/inventory/vehicles/add/+page.server.ts
@@ -2,6 +2,7 @@ import { unsecuredExternalApiRequest } from '$lib/scripts/httpRequests'
 import { HttpMethods } from '$lib/types/httpMethods'
 import type { Actions } from './$types'
 import { INVENTORY_URL } from '$lib/scripts/urls'
+import { redirect } from '@sveltejs/kit'
 
 export const actions = {
   default: async ({ request }) => {
@@ -25,5 +26,6 @@ export const actions = {
       HttpMethods.POST,
       body
     )
+    throw redirect(303, '/inventory/vehicles')
   },
 } satisfies Actions
