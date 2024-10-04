6ec95b93252c44bd84efaab75d5e1f809629209c
Maciej Lisowski <macieejl00@gmail.com>
2024-10-04 17:37:22 +0200

    replace inventory stores with generic ones

--- apps/erp-fe/src/lib/inventory/stores/selectedCategories.ts
+++
@@ -1,5 +0,0 @@
-import { writable } from 'svelte/store'
-
-let storeInit: string[] = []
-
-export const categoriesStore = writable(storeInit)
--- apps/erp-fe/src/lib/inventory/stores/selectedParts.ts
+++
@@ -1,5 +0,0 @@
-import { writable } from 'svelte/store'
-
-let storeInit: string[] = []
-
-export const partsStore = writable(storeInit)
