32a89c7724f2f5d9f0afbfcb0bf49b22845aad7d
Maciej Lisowski <macieejl00@gmail.com>
2024-10-20 16:10:02 +0200

    handle periodical money operation delete

--- apps/erp-fe/src/lib/finances/money-operation/periodical/PeriodicalMoneyOperationTable.svelte
+++ apps/erp-fe/src/lib/finances/money-operation/periodical/PeriodicalMoneyOperationTable.svelte
@@ -1,5 +1,56 @@
 <script lang="ts">
+  import { onDestroy, onMount } from 'svelte'
+  import { genericStore } from '$lib/stores/genericStore.ts'
+
   export let data: any = undefined
+  let selectedPeriodicals: Map<string, string> = new Map()
+
+  onMount(() => {
+    if (
+      $genericStore.finances !== undefined &&
+      $genericStore.finances.periodicals !== undefined
+    ) {
+      $genericStore.finances.periodicals.forEach((catg) =>
+        selectedPeriodicals.set(catg.uuid, catg)
+      )
+    }
+  })
+
+  onDestroy(() => {
+    $genericStore.finances = {}
+    $genericStore.finances.periodicals = Array.from(
+      selectedPeriodicals.values()
+    )
+  })
+
+  const updatePeriodicalsList = (periodical: string) => {
+    if (selectedPeriodicals.has(periodical.uuid)) {
+      selectedPeriodicals.delete(periodical.uuid)
+    } else {
+      selectedPeriodicals.set(periodical.uuid, periodical)
+    }
+    selectedPeriodicals = selectedPeriodicals
+    $genericStore.finances = {}
+    $genericStore.finances.periodicals = Array.from(
+      selectedPeriodicals.values()
+    )
+  }
+
+  const periodicalSelectedStyles = (
+    periodicalMap: Map<string, string>,
+    periodicalId: string
+  ): string => {
+    if (!periodicalMap.has(periodicalId)) return ''
+    return 'bg-indigo-600 text-white'
+  }
+
+  const determineEvenBgColor = (
+    periodicalMap: Map<string, string>,
+    periodicalId: string
+  ): string => {
+    if (!periodicalMap.has(periodicalId)) return 'even:bg-black'
+    return 'even:bg-indigo-600'
+  }
 </script>
 
 <div class="overflow-x-auto text-primary-content mx-auto">
@@ -20,7 +71,12 @@
     <tbody>
       {#if data !== undefined}
         {#each data.content as periodical}
-          <tr>
+          <tr
+            class={`hover:bg-indigo-400 hover:text-black even:text-white hover:even:text-black hover:even:bg-indigo-400 cursor-pointer ease-in transition-all duration-200
+        ${periodicalSelectedStyles(selectedPeriodicals, periodical.uuid)}
+        ${determineEvenBgColor(selectedPeriodicals, periodical.uuid)}`}
+            on:click={() => updatePeriodicalsList(periodical)}
+          >
             <td>{periodical.uuid}</td>
             <td>{periodical.operationCategory.operationType}</td>
             <td>{periodical.operationCategory.operationName}</td>
