d6b396b4d51d31f3874e364d6dad93aa8600dae2
Maciej Lisowski <macieejl00@gmail.com>
2024-11-24 14:26:44 +0100

    validate addresses

--- apps/erp-fe/src/routes/inventory/addresses/+page.svelte
+++ apps/erp-fe/src/routes/inventory/addresses/+page.svelte
@@ -27,16 +27,19 @@
   const updateConfig = () => {
     if ($genericStore.inventory === undefined) return
 
-    if (
-      $genericStore.inventory.addresses !== undefined &&
-      $genericStore.inventory.addresses.length > 0
-    ) {
+    if ($genericStore.inventory.addresses === undefined) {
+      config.editButton.disabled = true
+      return
+    }
+
+    if ($genericStore.inventory.addresses.length === 1) {
       config.editButton.url = `/inventory/addresses/${$genericStore.inventory.addresses[0].id}/edit`
       config.editButton.disabled = false
       config = config
-    } else {
-      config.editButton.disabled = true
+      return
     }
+
+    config.editButton.disabled = true
   }
 
   $: $genericStore, updateConfig()
