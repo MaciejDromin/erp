41c1ae7265709e6ffd5344180f2acf19a4d5847e
Maciej Lisowski <macieejl00@gmail.com>
2024-12-03 15:07:57 +0100

    checkstyle

--- apps/finances/src/main/java/com/soitio/finances/plannedexpenses/application/port/PlannedExpensesRepository.java
+++ apps/finances/src/main/java/com/soitio/finances/plannedexpenses/application/port/PlannedExpensesRepository.java
@@ -1,11 +1,9 @@
 package com.soitio.finances.plannedexpenses.application.port;
 
 import com.soitio.finances.plannedexpenses.domain.PlannedExpenses;
-
 import java.time.Month;
 import java.util.List;
 import java.util.Set;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
