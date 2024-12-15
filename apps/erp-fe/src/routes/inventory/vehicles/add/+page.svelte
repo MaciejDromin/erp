bb89026ceffc0bbe618bef0fb9c3f3905baed7ff
Maciej Lisowski <macieejl00@gmail.com>
2024-12-15 13:09:33 +0100

    Handle vehicle

--- apps/erp-fe/src/routes/inventory/vehicles/add/+page.svelte
+++ apps/erp-fe/src/routes/inventory/vehicles/add/+page.svelte
@@ -1,8 +1,10 @@
 <script>
   import VehicleAddView from '$lib/inventory/vehicles/VehicleAddView.svelte'
+
+  export let form
 </script>
 
 <div class="p-8">
   <h1 class="text-4xl font-bold text-black mb-8">Add Vehicle</h1>
-  <VehicleAddView />
+  <VehicleAddView {form} />
 </div>
