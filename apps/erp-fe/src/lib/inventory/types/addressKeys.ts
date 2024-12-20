4ee147ea3377f5e0373266ec05d7bbe241973e30
Maciej Lisowski <macieejl00@gmail.com>
2024-12-20 13:28:24 +0100

    Handle address

---
+++ apps/erp-fe/src/lib/inventory/types/addressKeys.ts
@@ -0,0 +1,28 @@
+const addressKeys = {
+  id: {
+    data: ['address', 'id'],
+    form: ['id', 'val'],
+  },
+  addressLine: {
+    data: ['address', 'addressLine'],
+    form: ['addressLine', 'val'],
+  },
+  city: {
+    data: ['address', 'city'],
+    form: ['city', 'val'],
+  },
+  postalCode: {
+    data: ['address', 'postalCode'],
+    form: ['postalCode', 'val'],
+  },
+  province: {
+    data: ['address', 'province'],
+    form: ['province', 'val'],
+  },
+  country: {
+    data: ['address', 'country'],
+    form: ['country', 'val'],
+  },
+}
+
+export default addressKeys
