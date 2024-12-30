c94f83410613508ec23d83d6f0c10a32b0828ac5
Maciej Lisowski <macieejl00@gmail.com>
2024-12-30 14:10:34 +0100

    prettier

--- apps/erp-fe/src/lib/scripts/urls.ts
+++ apps/erp-fe/src/lib/scripts/urls.ts
@@ -2,8 +2,4 @@ const CONSUL_URL = 'http://erp-consul:8500'
 const GATEWAY_URL = 'http://localhost:8070/api'
 const REPORTS_WS = 'ws://reports.soitio-erp.com:8000'
 
-export {
-  CONSUL_URL,
-  GATEWAY_URL,
-  REPORTS_WS,
-}
+export { CONSUL_URL, GATEWAY_URL, REPORTS_WS }
