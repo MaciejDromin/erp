a02433a42d94b06dc86b69c37254afadb72f1edf
Maciej Lisowski <macieejl00@gmail.com>
2025-01-20 14:13:21 +0100

    Multi tenancy support widgets finances

--- apps/widgets-finances/src/main/java/com/soitio/widgets/finances/application/FiltersDataService.java
+++ apps/widgets-finances/src/main/java/com/soitio/widgets/finances/application/FiltersDataService.java
@@ -2,10 +2,9 @@ package com.soitio.widgets.finances.application;
 
 import com.soitio.widgets.finances.client.FiltersDataClient;
 import jakarta.enterprise.context.ApplicationScoped;
-import org.eclipse.microprofile.rest.client.inject.RestClient;
-
 import java.time.Month;
 import java.util.List;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 @ApplicationScoped
 public class FiltersDataService {
@@ -16,12 +15,12 @@ public class FiltersDataService {
         this.filtersDataClient = filtersDataClient;
     }
 
-    public List<Integer> getBalanceYears() {
-        return filtersDataClient.getDistinctYears();
+    public List<Integer> getBalanceYears(String orgId) {
+        return filtersDataClient.getDistinctYears(orgId);
     }
 
-    public List<Month> getBalanceMonths(int year) {
-        return filtersDataClient.getDistinctMonths(year);
+    public List<Month> getBalanceMonths(int year, String orgId) {
+        return filtersDataClient.getDistinctMonths(orgId, year);
     }
 
 }
--- apps/widgets-finances/src/main/java/com/soitio/widgets/finances/web/WidgetsController.java
+++ apps/widgets-finances/src/main/java/com/soitio/widgets/finances/web/WidgetsController.java
@@ -3,11 +3,11 @@ package com.soitio.widgets.finances.web;
 import com.soitio.widgets.common.domain.data.WidgetData;
 import com.soitio.widgets.finances.application.WidgetService;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.HeaderParam;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.QueryParam;
-import lombok.RequiredArgsConstructor;
-
 import java.time.Month;
+import lombok.RequiredArgsConstructor;
 
 @Path("/widgets")
 @RequiredArgsConstructor
@@ -17,39 +17,40 @@ public class WidgetsController {
 
     @GET
     @Path("/total-net-worth")
-    public WidgetData getNetWorth() {
-        return widgetService.calculateTotalNetWorth();
+    public WidgetData getNetWorth(@HeaderParam("X-OrgId") String orgId) {
+        return widgetService.calculateTotalNetWorth(orgId);
     }
 
     @GET
     @Path("/monthly-balance")
-    public WidgetData getMonthlyBalance() {
-        return widgetService.calculateMonthlyBalance();
+    public WidgetData getMonthlyBalance(@HeaderParam("X-OrgId") String orgId) {
+        return widgetService.calculateMonthlyBalance(orgId);
     }
 
     @GET
     @Path("/monthly-balance-diff")
     public WidgetData getMonthlyBalanceDiff(@QueryParam("year") int year,
-                                            @QueryParam("month") Month month) {
-        return widgetService.calculateMonthlyBalanceDiff(year, month);
+                                            @QueryParam("month") Month month,
+                                            @HeaderParam("X-OrgId") String orgId) {
+        return widgetService.calculateMonthlyBalanceDiff(year, month, orgId);
     }
 
     @GET
     @Path("/most-expensive-item-per-category")
-    public WidgetData getMostExpensiveItemPerCat() {
-        return widgetService.getMostExpensiveItemPerCat();
+    public WidgetData getMostExpensiveItemPerCat(@HeaderParam("X-OrgId") String orgId) {
+        return widgetService.getMostExpensiveItemPerCat(orgId);
     }
 
     @GET
     @Path("/value-per-category")
-    public WidgetData getValuePerCategory() {
-        return widgetService.getValuePerCategory();
+    public WidgetData getValuePerCategory(@HeaderParam("X-OrgId") String orgId) {
+        return widgetService.getValuePerCategory(orgId);
     }
 
     @GET
     @Path("/remaining-planned-expenses")
-    public WidgetData getRemainingPlannedExpenses() {
-        return widgetService.getRemainingPlannedExpenses();
+    public WidgetData getRemainingPlannedExpenses(@HeaderParam("X-OrgId") String orgId) {
+        return widgetService.getRemainingPlannedExpenses(orgId);
     }
 
 }
