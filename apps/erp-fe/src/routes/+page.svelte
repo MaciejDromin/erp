3f89a780a02b82793b5652d6ac59ca1cdb3ea9c3
Maciej Lisowski <macieejl00@gmail.com>
2025-01-22 17:35:14 +0100

    moved directories

--- apps/erp-fe/src/routes/+page.svelte
+++
@@ -1,4 +0,0 @@
-<div class="text-primary-content w-full h-full">
-  <h1>Hey!</h1>
-  <p>Hey!</p>
-</div>
--- apps/erp-fe/src/routes/finances/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-  import DashboardWrapper from '$lib/DashboardWrapper.svelte'
-</script>
-
-<div class="text-primary-content w-full h-full p-3">
-  <DashboardWrapper url="/dashboards/types/FINANCES" domain="FINANCES" />
-</div>
