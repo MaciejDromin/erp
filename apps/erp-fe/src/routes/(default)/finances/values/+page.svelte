7e25dc1968b3de83b45b18c68c72e2fc7e017862
Maciej Lisowski <macieejl00@gmail.com>
2025-01-29 13:47:38 +0100

    Values page and styling

--- apps/erp-fe/src/routes/(default)/finances/values/+page.svelte
+++ apps/erp-fe/src/routes/(default)/finances/values/+page.svelte
@@ -1,1 +1,29 @@
-<!-- TODO: create content -->
+<div class="text-primary-content w-full h-full p-6">
+  <div class="flex flex-col mx-auto w-fit mt-12">
+    <h1 class="text-3xl font-bold mx-auto w-fit">
+      Define Values of Your Resources!
+    </h1>
+    <p class="mt-12 w-[768px]">
+      In this section you will be able to determine the approximate value of all
+      your resources. Follow the links below to explore and add your values into
+      the system.
+    </p>
+    <ul class="list-inside list-disc mt-6">
+      <li>
+        <a class="text-[$131515] font-bold" href="/finances/values/items"
+          >Items</a
+        >
+      </li>
+      <li>
+        <a class="text-[$131515] font-bold" href="/finances/values/properties"
+          >Properties</a
+        >
+      </li>
+      <li>
+        <a class="text-[$131515] font-bold" href="/finances/values/vehicles"
+          >Vehicles</a
+        >
+      </li>
+    </ul>
+  </div>
+</div>
